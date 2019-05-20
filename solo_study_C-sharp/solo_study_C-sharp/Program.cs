6dcbd37824cce5c2a7206c7c687455523d168ea2
bitrate256 <bitrate256@gamil.com>
2019-05-20 15:03:37 +0900

    dd

---
+++ solo_study_C-sharp/solo_study_C-sharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace solo_study_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+// 저장소 새로 생성 후 업로드.
