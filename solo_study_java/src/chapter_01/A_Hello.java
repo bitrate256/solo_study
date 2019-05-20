1db50ec477ea4776f79fbb446a05866b1df820d8
bitrate256 <bitrate256@gamil.com>
2019-05-20 11:52:19 +0900

    again

---
+++ solo_study_java/src/chapter_01/A_Hello.java
@@ -0,0 +1,14 @@
+package chapter_01;
+
+public class A_Hello {
+
+	public static void main(String[] args) {
+		// 타입 먼저, 그 다음이 변수의 이름이다.
+		// = (대입연산자)
+		int A = 10; // 변수 선언
+		System.out.println(10); // 출력
+		System.out.println(A);
+
+	}
+
+}
