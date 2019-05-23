24f35537ff251e8f36a8f77cf5cef938b7ba3d16
bitrate256 <bitrate256@gamil.com>
2019-05-23 11:51:59 +0900

    잘못된 내용들 수정. 이전 커밋에서 고친건 변수2번에 관한 언급임. 2번이 아니라 1번을 고친 것. 착오로 인해 4번에 3번 내용이 중복해서 올라갔고, 3번을 알기쉽게 고치고 4번 내용 추가함.

--- solo_study_java/src/chapter_01/B_variable_03.java
+++ solo_study_java/src/chapter_01/B_variable_03.java
@@ -8,8 +8,8 @@ public class B_variable_03 {
      */
      //지역변수는 반드시 메소드 안에서 선언되거나 파라미터 자리에 선언해야 함
     public void methodA() {
-           int j;//지역변수임 - 지변은 선언과 초기화를 나누어 작성할 수 있음.
-           j = 20;
+    	int j;//지역변수임 - 지변은 선언과 초기화를 나누어 작성할 수 있음.
+    	j = 20;
     }
     /*
      * 로그인을 사용자(업무담당자)가 입력하면 그 값이 오라클 서버에 전달되어야 함.
@@ -18,11 +18,11 @@ public class B_variable_03 {
      * WHERE mem_id=? AND mem_pw=?
      */
     public String loginAction(String mem_id, String mem_pw) {
-           return "이순신님 환영합니다.";
+    	return mem_id + "계정으로 로그인 하였습니다.";
     }
     public static void main(String[] args) {
-           B_variable_03 p = new B_variable_03();
-           String mem_name = p.loginAction("test", "123");
-           System.out.println(mem_name);
+    	B_variable_03 p = new B_variable_03();
+    	String mem_name = p.loginAction("test", "123");
+    	System.out.println(mem_name + "님 환영합니다.");
     }
 }
--- solo_study_java/src/chapter_01/B_variable_04.java
+++ solo_study_java/src/chapter_01/B_variable_04.java
@@ -1,28 +1,50 @@
 package chapter_01;
+/*
+변수 선언 위치에 따른 변수의 종류
+전역변수
+지역변수
 
+
+인스턴스변수(참조형변수)
+
+원시형변수
+참조형변수
+
+자바에서는 같은 이름의 메소드가 중복되어도 에러가 발생하지 않는다.
+ */
+import java.util.Scanner; // util 패키지에서 Scanner 클래스 불러옴
+/*
+연습문제
+달의 중력은 지구 중력의 17%다.
+지구에서 몸무게가 100kg인 사람은 달에 가면 17kg다.
+당신의 몸무게는 달에서 얼마인가?
+몸무게 n은 실수이고 키보드로부터 입력받는다.
+
+여기서 지변으로 사용할 변수는 무엇인가?
+전역변수로 사용하는 경우가 차이가 있을것인가?
+
+너의 몸무게를 입력받고 싶어요 -> 변수가 필요하다 -> 전역 & 지역변수 선택
+키보드로 몸무게를 어떻게 입력받지? -> 안배웠다
+연산자가 필요함 -> 오라클 패스
+(몸무게x17)/100
+예) 100*17/100 -> 시뮬레이션 해보기 -> 테스트 -> 검증
+
+클래스 이름을 뭘로 할것인가? com.ch2.Moonlight.java
+
+코딩 시작
+ */
 public class B_variable_04 {
-    // 전역변수는 선언과 초기화를 반드시 한번에 해야 함.
-    int j = 10;
-    /* int j; 에러임, 왜냐하면 선언과 초기화를 나누어 할 수 없다.(컴파일 에러)
-     * j = 20;
-     */
-     //지역변수는 반드시 메소드 안에서 선언되거나 파라미터 자리에 선언해야 함
-    public void methodA() {
-    	int j;//지역변수임 - 지변은 선언과 초기화를 나누어 작성할 수 있음.
-    	j = 20;
-    }
-    /*
-     * 로그인을 사용자(업무담당자)가 입력하면 그 값이 오라클 서버에 전달되어야 함.
-     * 오라클에서 아래 select문 실행[요청]
-     * select mem_name FROM member
-     * WHERE mem_id=? AND mem_pw=?
-     */
-    public String loginAction(String mem_id, String mem_pw) {
-    	return mem_id + "계정으로 로그인 하였습니다.";
-    }
-    public static void main(String[] args) {
-    	B_variable_04 p = new B_variable_04();
-    	String mem_name = p.loginAction("test", "123");
-    	System.out.println(mem_name + "님 환영합니다.");
-    }
+       public static void main(String[] args) {
+              Scanner scan = new Scanner(System.in); // 메인 메소드 안에 new 명령 사용해 Scanner 객체 선언함
+                                                     // 여기서는 scan 이라는 이름의 객체를 선언함.
+        System.out.print("몸무게를 입력하세요.:");
+              String input = scan.nextLine();
+              int num = Integer.parseInt(input); // 입력받은 문자열을 숫자로 변환함
+              scan.close(); // 스캔 입력받는 것을 멈춤
+              
+              double result; // 결과값
+              result = (num * 17)/100;
+              
+              System.out.println("몸무게는" + result + "입니다.");
+       }
 }
